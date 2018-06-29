ee4ff83e4c506fd2ac3acb0266bbc1bc76261cb2
brfreitag <brufreitas@gmail.com>
2018-06-29 01:06:40 -0300

    Entrada pode ser login ou uuid

--- classes/user.class.php
+++ classes/user.class.php
@@ -5,17 +5,25 @@ class user
   public $login;
   public $uuid;
 
-  public function __construct($login, $pass) {
+  public function __construct($login_uuid) {
+    if (ctype_xdigit($login_uuid)) {
+      $w = "userUUID = 0x{$login_uuid}";
+    } else {
+      $w = "login = '{$login_uuid}'";
+    }
+
     // @todo ajustar o modo de fazer o login
-    $q = "SELECT HEX(userUUID) AS userUUID FROM tb_user WHERE login = '{$login}' AND pass = '{$pass}'";
+    $q = "SELECT HEX(userUUID) AS userUUID, login FROM tb_user WHERE {$w}";
+
     $con = new ConexaoLocal();
     $con->query($q);
     if ($con->count == 0) {
-      echo "Não reconheço {$login}\n";
+      echo "Não reconheço login {$login_uuid}\n";
       unset($con);
       return false;
     }
-    $this->login = $login;
+
+    $this->login = $con->result["login"];
     $this->uuid = $con->result["userUUID"];
     unset($con);
     return true;
