790e1d4de1851f72132ac115e977da4031e8f0bf
brfreitag <brufreitas@gmail.com>
2018-07-22 22:25:29 -0300

    Pgm teste para verificar aleatoriedade da função random_int

---
+++ testes/random_int.php
@@ -0,0 +1,21 @@
+<?php
+$max = 10; // number of random values
+$test = 1000000;
+
+$array = array_fill(0, $max, 0);
+
+for ($i = 0; $i < $test; ++$i) {
+    ++$array[random_int(0, $max-1)];
+}
+
+function arrayFormatResult(&$item) {
+    global $test, $max; // try to avoid this nowdays ;)
+    
+    $perc = ($item/($test/$max))-1;
+    $item .= ' '. number_format($perc, 4, '.', '') .'%';
+}
+
+array_walk($array, 'arrayFormatResult');
+
+print_r($array);
+?>
